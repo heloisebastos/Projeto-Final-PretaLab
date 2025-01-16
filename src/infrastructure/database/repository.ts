12178f0f8fad3e258c48a416e7f6a32eee5fae11
heloisebastos <heloisebastos28@hotmail.com>
2025-01-16 15:42:21 -0400

    feat: adiciona implementação dos métodos `save` e `findAll`

--- src/infrastructure/database/repository.ts
+++ src/infrastructure/database/repository.ts
@@ -3,5 +3,29 @@ import { Despesa } from '../../domain/despesa';
 import { DespesaModel } from './model';
 
 export class RepositoryData implements DespesaRepository {
-    //seu codigo aqui
+    async save(despesa: Despesa): Promise<void> {
+        const despesaModel = new DespesaModel(despesa);
+        await despesaModel.save();
+    }
+
+    async findAll(): Promise<Despesa[]> {
+        const despesa = await DespesaModel.find()
+        const translatedDespesas = despesa.map(item => {
+            return {
+                id: item._id.toString(),
+                descricao: item.descricao,
+                valor: item.valor,
+                tipo: item.tipo,
+                data: item.data,
+                userId: item.userId,
+                categoria: item.categoria
+
+            }
+        }) as Despesa[];
+
+        return translatedDespesas;
+
+    }
+
+
 }
